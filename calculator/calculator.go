b5c73a5fb8629b202db5f7bd156bfeff383e20e6
Botond <g.pont.botond@gmail.com>
2024-11-08 21:10:05 +0100

    Division
    Functionality and error handling

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,5 +1,7 @@
 package calculator
 
+import "errors"
+
 func Add(a, b float64) float64 {
 	return a + b
 }
@@ -10,4 +12,12 @@ func Subtract(a, b float64) float64 {
 
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("Divison by zero is weird")
+	}
+
+	return a / b, nil
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -27,11 +27,23 @@ func TestAdd(t *testing.T) {
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+
+	type testCase struct {
+		a, b, want float64
 	}
+
+	testCases := []testCase{
+		{a: 1, b: 1, want: 0},
+		{a: 5, b: 1, want: 4},
+		{a: 1, b: 5, want: -4},
+	}
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
+	}
+
 }
 
 func TestMultiply(t *testing.T) {
@@ -40,5 +52,40 @@ func TestMultiply(t *testing.T) {
 	got := calculator.Multiply(2, 5)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 1},
+		{a: 1, b: 1, want: 1},
+		{a: 5, b: 1, want: 5},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+
+		if err != nil {
+			t.Fatalf("want no error for valid input, got %v", err)
+		}
+
+		if tc.want != got {
+			t.Errorf("Add( %f %f): want %f, got %f",
+				tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestDivideInvalid(t *testing.T) {
+	t.Parallel()
+	_, err := calculator.Divide(2, 0)
+
+	if err != nil {
+		t.Fatalf("error happened : %v", err)
 	}
 }
