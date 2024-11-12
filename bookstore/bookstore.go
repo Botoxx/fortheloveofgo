f2c1a2ff13ca18946f295ba7f408ba0ab993d8c9
Botond <g.pont.botond@gmail.com>
2024-11-12 21:39:32 +0100

    Implement Buy function
    plus relating positive and negative testcases

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,7 +1,17 @@
 package bookstore
 
+import "errors"
+
 type Book struct {
 	Title  string
 	Author string
 	Copies int
+}
+
+func Buy(b Book) (Book, error) {
+	if b.Copies > 0 {
+		b.Copies--
+		return b, nil
+	}
+	return Book{}, errors.New("out of stock")
 }
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -13,3 +13,34 @@ func TestBook(t *testing.T) {
 		Copies: 3,
 	}
 }
+
+func TestBuy1(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Lucy Liu",
+		Copies: 2,
+	}
+	want := 1
+	result, err := bookstore.Buy(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := result.Copies
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestBuy2(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Lucy Liu",
+		Copies: 0,
+	}
+	_, err := bookstore.Buy(b)
+	if err == nil {
+		t.Error("want error buying from zero copies, got nil")
+	}
+}
